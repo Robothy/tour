d9a4631762b801137cc2265efd6156006916230c
robothy <robothyluo@gmail.com>
2017-07-01 11:10:56 +0800

    删除多余的文件

--- src/tree/binary_search_tree.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BINARY_SEARCH_TREE_HPP
-#define BINARY_SEARCH_TREE_HPP
-
-typedef struct TouristAttraction Attraction, *pAttraction;
-
-
-#include "binary_search_tree.cpp"
-
-#endif // BINARY_SEARCH_TREE_HPP
