d6ee43a3b060d76cca4145f71891cb4b9f30f70a
robothy <robothyluo@gmail.com>
2017-07-02 18:29:47 +0800

    新增用户单链表

---
+++ src/tree/singly_linked_list_user.c
@@ -0,0 +1,66 @@
+#include "singly_linked_list_user.h"
+
+int InsertUser(pUser *root, pUser u){
+    if((*root) == NULL){
+        (*root) = u;
+    }
+    else{
+        pUser p = (*root);
+        while(p->next != NULL){
+            if(strcmp(p->id , u->id)==0){
+                return 0;
+            }
+            p = p->next;
+        }
+        if(strcmp(p->id, u->id) == 0){
+            return 0;
+        }
+        p->next = u;
+    }
+    return 1;
+}
+
+int DeleteUser(pUser *root, char *id){
+    if((*root) == NULL) {
+        return 0;
+    }
+    pUser p = (*root);
+    if(strcmp((*root)->id, id) == 0){
+        (*root) = (*root)->next;
+    }else{
+        while(p->next != NULL){
+            if(strcmp(p->next->id,id) == 0){//ÕÒµ½´ýÉ¾³ý½Úµã
+                p->next = p->next->next;
+                p = p->next;
+                free(p);
+                return 1;
+            }
+            p = p->next;
+        }
+    }
+    return 0;
+}
+
+pUser SearchUser(pUser root, char*id){
+    while(root!=NULL){
+        if(strcmp(root->id, id) == 0) break;
+        root = root->next;
+    }
+    return root;
+}
+
+void DisplayUser(pUser u){
+    if(u == NULL){
+        printf("Can not display User, because it is NULL\n");
+    }
+    else{
+        printf("%-5s %-20s %-3c %-5d\n",u->id, u->name, u->sex, u->age);
+    }
+}
+
+void DisplayAllUser(pUser root){
+    while(root){
+        DisplayUser(root);
+        root = root->next;
+    }
+}
---
+++ src/tree/singly_linked_list_user.h
@@ -0,0 +1,34 @@
+/** 存储用户信息的单链表 */
+/** 用户的唯一标识信息为ID, 即ID不同，其它信息相同的用户视为不同的用户 */
+
+#ifndef SINGLY_LINKED_LIST_USER_H
+#define SINGLY_LINKED_LIST_USER_H
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Usr{
+    char id[4];
+    char name[30];
+    char sex;
+    short age;
+    struct Usr *next;
+} User, *pUser;
+
+/** 插入用户 */
+int InsertUser(pUser*, pUser);
+
+/** 删除用户 */
+int DeleteUser(pUser*, char*);
+
+/** 查找用户 */
+pUser SearchUser(pUser, char*);
+
+/** 打印单个用户信息 */
+void DisplayUser(pUser);
+
+/** 打印所有用户信息 */
+void DisplayAllUser(pUser);
+
+
+#endif // SINGLY_LINKED_LIST_USER_H
