2b84709f05bf5c0fbdbc2f1ad5c6c7b4a61fde96
robothy <robothyluo@gmail.com>
2017-07-02 11:45:26 +0800

    二叉排序树增、删、查函数编写完成

--- src/tree/binary_search_tree.c
+++ src/tree/binary_search_tree.c
@@ -1,17 +1,6 @@
 #include "binary_search_tree.h"
 
 /**
-*   定义【旅游景点】节点，包含景点名称，出发时间，人数
-*/
-struct TouristAttraction{
-    char name[50];                  //tourist attraction's name
-    char go_off[10];                //go-off, format: yyyy-mm-dd
-    unsigned int tourists_number;   // The number of tourists
-    struct TouristAttraction *lchild;//Pointer of left child
-    struct TouristAttraction *rchild;//Pointre of right child
-};
-
-/**
 *   功能： 搜索二叉树
 *   参数： pAttraction root 二叉排序树的根节点
 *          char *name      景点名称
@@ -25,10 +14,10 @@ pAttraction BSTSearch(pAttraction root,char *name, char *go_off){
     }
     else if(strcmp(root->name, name)<0 ||
             (strcmp(root->name, name)==0 && strcmp(root->go_off, go_off)<0)){
-        return BSTSearch(root->lchild, name, go_off);
+        return BSTSearch(root->rchild, name, go_off);
     }
     else{
-        return BSTSearch(root->rchild, name, go_off);
+        return BSTSearch(root->lchild, name, go_off);
     }
 }
 
@@ -39,16 +28,76 @@ pAttraction BSTSearch(pAttraction root,char *name, char *go_off){
 *           pAttaction e        待插入的节点
 *
 */
-
-void BSTInsert(pAttraction root, pAttraction e){
-    if(root == NULL){
-        root = e;
+void BSTInsert(pAttraction *root, pAttraction e){
+    if((*root) == NULL || e == NULL){
+        (*root) = e;
     }
-    else if(strcmp(root->name,e->name)<0 ||
-    (strcmp(root->name,e->name)==0 && strcmp(root->go_off,e->go_off)<0)){
-        BSTInsert(root->lchild, e);
+    else if(compare_node(*root,e) < 0){ //应该放到右边
+        BSTInsert(&((*root)->rchild), e);
+    }else if(compare_node(*root, e) > 0){ //应该放到左边
+        BSTInsert(&((*root)->lchild), e);
+    }else{  //不插入,提示警告信息
+        printf("Can not insert, The node with name %s , go_off %s already exists.\n",e->name, e->go_off);
+    }
+}
+
+/** 删除节点 ***/
+int BSTDelete(pAttraction*root, char* name, char* go_off, pAttraction *parent){
+
+    if(root == NULL){
+        return 0;
+    }
+    if((strcmp((*root)->name,name)==0 && strcmp((*root)->go_off, go_off)==0)){//找到了待删除的节点
+        pAttraction l = (*root)->lchild;
+        pAttraction r = (*root)->rchild;
+        pAttraction self = *root;    //待删除的节点
+        if(parent == NULL){ //树的根节点即为要删除的节点
+            if(l == NULL){
+                *root = r;
+            }else{
+                BSTInsert(&l, r);   //将右子树合并到左子树上
+                *root = l;
+            }
+        }else{
+            if(l == NULL) (*root) = r;
+            else if(r == NULL) (*root) = l;
+            else{   //左右子树均不为空
+                pAttraction temp = r;
+                while(temp->lchild!=NULL){
+                    temp = temp->lchild;
+                }
+                temp->lchild = l;   //将左子树放到右子树的最左边
+                (*root) = r;
+            }
+        }
+        free(self);//释放内存
+        return 1;
+    }
+    else if(strcmp((*root)->name, name)<0 ||
+            (strcmp((*root)->name, name)==0 && strcmp((*root)->go_off, go_off)<0)){
+        return BSTDelete(&((*root)->rchild), name, go_off, root);
     }
     else{
-        BSTInsert(root->rchild, e);
+        return BSTDelete(&((*root)->lchild), name, go_off, root);
+    }
+}
+
+
+/**
+*   自定义比较两个节点大小的规则
+**/
+int compare_node(pAttraction l, pAttraction r){
+    if(strcmp(l->name,r->name) > 0){    //景点名称左边大于右边
+        return 1;
+    }else if(strcmp(l->name, r->name) < 0 ){    //景点名称左边小于右边
+        return -1;
+    }else{  //景点名称左右两边相等， 比较出发时间
+        if(strcmp(l->go_off, r->go_off)>0){
+            return 1;
+        }else if(strcmp(l->go_off, r->go_off)<0){
+            return -1;
+        }else{
+            return 0;
+        }
     }
 }
--- src/tree/binary_search_tree.h
+++ src/tree/binary_search_tree.h
@@ -1,14 +1,25 @@
-#ifndef BINARY_SEARCH_TREE_HPP
-#define BINARY_SEARCH_TREE_HPP
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
 
 #include <string.h>
 #include "singly_linked_list.h"
 
-typedef struct TouristAttraction  Attraction, *pAttraction;
+/**
+*   定义【旅游景点】节点，包含景点名称，出发时间，人数
+*/
+typedef struct TouristAttraction{
+    char name[50];                  //tourist attraction's name
+    char go_off[11];                //go-off, format: yyyy-mm-dd
+    unsigned int tourists_number;   // The number of tourists
 
+    struct TouristAttraction *lchild;//Pointer of left child
+    struct TouristAttraction *rchild;//Pointre of right child
+}Attraction, *pAttraction;
+
+//typedef struct TouristAttraction  Attraction, *pAttraction;
+int BSTDelete(pAttraction *root, char*,char*,pAttraction*);
 pAttraction BSTSearch(pAttraction root,char *name, char *go_off);
-void BSTInsert(pAttraction root, pAttraction e);
+void BSTInsert(pAttraction *root, pAttraction e);
+int compare_node(pAttraction,pAttraction);
 
-#include "binary_search_tree.c"
-
-#endif // BINARY_SEARCH_TREE_HPP
+#endif // BINARY_SEARCH_TREE_H
